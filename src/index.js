baf41edb4a542b1a4e1faf928866abd11de837d3
Marc Lorente <mlorente2005@gmail.com>
2024-06-20 12:58:04 +0200

    Create theme selector and add styles to it

--- src/index.js
+++ src/index.js
@@ -1,4 +1,43 @@
-function sum(a, b) {
-    return a + b;
+window.onload = () => {
+    setColorScheme();
+};
+
+function setColorScheme() {
+    const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)");
+    console.log(prefersDarkScheme);
+    let html = document.getElementsByTagName('html')[0];
+    console.log(html);
+    let themeSelector = document.getElementById('themeSelector');
+    let themeLabel = document.getElementById('themeLabel');
+    let moonIcon = document.getElementById('moonIcon');
+    let sunIcon = document.getElementById('sunIcon');
+
+    if (prefersDarkScheme.matches) {
+        console.log("dark");
+        html.classList.add('dark');
+        console.log(html.classList);
+        themeLabel.classList.add('after:left-[4px]');
+        moonIcon.classList.add("opacity-0");
+        sunIcon.classList.remove("opacity-0");
+    } else {
+        console.log("light");
+        html.classList.remove('dark');
+        themeLabel.classList.add('after:left-[45px]');
+        moonIcon.classList.add("opacity-0");
+        sunIcon.classList.remove("opacity-0");
+    }
+
+    themeSelector.addEventListener('click', () => {
+        if (html.classList.contains('dark')) {
+            html.classList.remove('dark');
+            themeLabel.classList.remove('after:left-[4px]');
+            moonIcon.classList.add("opacity-0");
+            sunIcon.classList.remove("opacity-0");
+        } else {
+            html.classList.add('dark');
+            themeLabel.classList.add('after:left-[4px]');
+            moonIcon.classList.remove("opacity-0");
+            sunIcon.classList.add("opacity-0");
+        }
+    });
 }
-module.exports = sum;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,js}"],
+  darkMode: 'selector',
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
