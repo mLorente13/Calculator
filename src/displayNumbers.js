985726bd7ce53a9b53d059b1d6b6d0d69d379cad
Marc Lorente <mlorente2005@gmail.com>
2024-07-09 13:05:42 +0200

    Add functionality to scroll display to see full result

--- src/displayNumbers.js
+++ src/displayNumbers.js
@@ -3,7 +3,11 @@ const seconDisplay = document.getElementById('operation');
 
 function updateDisplay(tile) {
     if (tile.dataset.value === 'backspace') return display.textContent = display.textContent.slice(0, -1) || '0';
-    if (tile.dataset.value === 'C') return display.textContent = '0';
+    if (tile.dataset.value === 'C') {
+        display.textContent = '0';
+        seconDisplay.textContent = '0';
+        return;
+    }
     if (tile.dataset.value === '=') return calculateOperation(display.textContent);
     if (display.textContent == '0' && tile.dataset.value !== '.') return display.textContent = tile.dataset.value;
     display.textContent += tile.dataset.value;
@@ -17,5 +21,31 @@ function calculateOperation(operation) {
     }
     seconDisplay.textContent = operation;
 }
+
+function scrollDisplay(direction) {
+    if (direction === 'ArrowLeft') {
+        display.scrollLeft -= 10;
+        seconDisplay.scrollLeft -= 10;
+    } else if (direction === 'ArrowRight') {
+        display.scrollLeft += 10;
+        seconDisplay.scrollLeft += 10;
+    }
+}
+
+window.addEventListener('keydown', (e) => {
+    if (e.key.includes('Arrow')) {
+        scrollDisplay(e.key);
+    }
+});
+
+display.addEventListener('wheel', (e) => {
+    if (e.deltaY > 0) {
+        display.scrollLeft += 10;
+        seconDisplay.scrollLeft += 10;
+    } else {
+        display.scrollLeft -= 10;
+        seconDisplay.scrollLeft -= 10;
+    }
+});
 
 export { updateDisplay };
